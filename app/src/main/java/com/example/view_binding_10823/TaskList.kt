db7db2642993027ac9c83f8dde666803bb5284e0
Justin <a>
2022-09-15 09:42:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10823/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10823
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari hari untuk membuat hari makin indah"),
+        Task("Chapter 2","Nguli Genshin Impact","Mencari prinsip gacha keqing terwangi"),
+        Task("Chapter 3","ngopi","Habis capek nguli"),
+        Task("Chapter 4","dimana mama ei?","Adu mekanik showcase Ayaka Pyro"),
+        Task("Chapter 5","Upload ke tiktok","Pamer AYaka Pyro c6 + bennet c6")
+    )
+}
